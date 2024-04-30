b1b20a8b9999677e36bcb17bfd95cc15e6973b9e
GodfreySam <godfreysam09@gmail.com>
2024-04-30 10:17:25 +0100

    fixed network status indicator

--- src/components/check-status.js
+++ src/components/check-status.js
@@ -2,37 +2,42 @@ import * as React from "react";
 import { showNetworkToast } from "../services/helpers";
 
 const getOnLineStatus = () =>
-  typeof navigator !== "undefined" && typeof navigator.onLine === "boolean"
-    ? navigator.onLine
-    : true;
+	typeof navigator !== "undefined" && typeof navigator.onLine === "boolean"
+		? navigator.onLine
+		: true;
 
 const useNavigatorOnLine = () => {
-  const [status, setStatus] = React.useState(getOnLineStatus());
+	const [status, setStatus] = React.useState(getOnLineStatus());
 
-  const setOnline = () => setStatus(true);
-  const setOffline = () => setStatus(false);
+	const setOnline = () => setStatus(true);
+	const setOffline = () => setStatus(false);
 
-  React.useEffect(() => {
-    window.addEventListener("online", setOnline);
-    window.addEventListener("offline", setOffline);
+	React.useEffect(() => {
+		window.addEventListener("online", setOnline);
+		window.addEventListener("offline", setOffline);
 
-    return () => {
-      window.removeEventListener("online", setOnline);
-      window.removeEventListener("offline", setOffline);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("online", setOnline);
+			window.removeEventListener("offline", setOffline);
+		};
+	}, []);
 
-  return status;
+	return status;
 };
 
 export const NetworkStatusIndicator = () => {
-  const isOnline = useNavigatorOnLine();
+	const isOnline = useNavigatorOnLine();
+	const isInitialRender = React.useRef(true);
 
-  React.useEffect(() => {
-    isOnline
-      ? showNetworkToast("Back online!", "success")
-      : showNetworkToast("Poor network detected", "warning");
-  }, [isOnline]);
+	React.useEffect(() => {
+		if (isInitialRender.current) {
+			isInitialRender.current = false;
+		} else {
+			isOnline
+				? showNetworkToast("Back online!", "success")
+				: showNetworkToast("Poor network detected", "warning");
+		}
+	}, [isOnline]);
 
-  return null;
+	return null;
 };
