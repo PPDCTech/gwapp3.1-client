d820f1928e44b41bcbcf7bd263aa2dd30b5df040
GodfreySam <godfreysam09@gmail.com>
2024-09-19 01:39:42 +0100

    optimisation batch

--- src/pages/complete-registration.js
+++ src/pages/complete-registration.js
@@ -13,12 +13,13 @@ import {
 	Select,
 	MenuItem,
 	OutlinedInput,
+	Modal,
 	IconButton,
 } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { toast } from "react-toastify";
 import { useParams, useNavigate } from "react-router-dom";
-import { updateProfile, viewAVendor } from "../services/vendor-api-Services";
+import { toast } from "react-toastify";
+import { viewAVendor, updateProfile } from "../services/vendor-api-Services";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const categoriesData = [
 	"Airlines & Travel Agents",
@@ -54,18 +55,22 @@ const categoriesData = [
 ];
 
 const CompleteRegistration = () => {
-	const { vendorId } = useParams();
 	const navigate = useNavigate();
 	const uniqueCategories = [...new Set(categoriesData)];
+	const [vendorId, setVendorId] = useState("");
 
 	useEffect(() => {
-		// reload the page on mount to fetch the vendor data
-		window.location.reload();
+		// search if vendor = true set method to vendor
+		const urlSearchParams = new URLSearchParams(window.location.search);
+		const vendor_id = urlSearchParams.get("vendorId");
+		if (vendor_id) {
+			setVendorId(vendor_id);
+		}
 	}, []);
 
-	const userId = vendorId;
-
+	const [step, setStep] = useState(1);
 	const [submitting, setSubmitting] = useState(false);
+	const [isModalOpen, setModalOpen] = useState(false);
 
 	const [accountDetails, setAccountDetails] = useState({
 		holderName: "",
@@ -90,15 +95,13 @@ const CompleteRegistration = () => {
 	});
 
 	const [categories, setCategories] = useState([]);
-
 	const [files, setFiles] = useState([]);
 	const [documentNames, setDocumentNames] = useState([]);
 
 	useEffect(() => {
-		// Fetch vendor data and populate state
 		const fetchVendorData = async () => {
 			try {
-				const response = await viewAVendor(userId);
+				const response = await viewAVendor(vendorId);
 				const vendor = response.data;
 				setAccountDetails(vendor?.accountDetails);
 				setContactPerson(vendor?.contactPerson);
@@ -106,12 +109,13 @@ const CompleteRegistration = () => {
 				setCategories(vendor?.categories);
 			} catch (error) {
 				toast.error("Failed to fetch vendor data.");
-				console.error("Failed to fetch vendor data:", error);
 			}
 		};
 
-		fetchVendorData();
-	}, [userId]);
+		if (vendorId) {
+			fetchVendorData();
+		}
+	}, [vendorId]);
 
 	const handleAccountDetailsChange = (e) => {
 		setAccountDetails({ ...accountDetails, [e.target.name]: e.target.value });
@@ -130,21 +134,18 @@ const CompleteRegistration = () => {
 		setCategories(typeof value === "string" ? value.split(",") : value);
 	};
 
-	// Handle file input changes
 	const handleFileChange = (event) => {
 		const newFiles = Array.from(event.target.files);
 		setFiles([...files, ...newFiles]);
-		setDocumentNames([...documentNames, ...newFiles.map(() => "")]); // Initialize empty names for new files
+		setDocumentNames([...documentNames, ...newFiles.map(() => "")]);
 	};
 
-	// Handle document name change
 	const handleDocumentNameChange = (index, event) => {
 		const updatedNames = [...documentNames];
 		updatedNames[index] = event.target.value;
 		setDocumentNames(updatedNames);
 	};
 
-	// Handle file removal
 	const handleRemoveFile = (index) => {
 		const updatedFiles = [...files];
 		updatedFiles.splice(index, 1);
@@ -156,24 +157,15 @@ const CompleteRegistration = () => {
 		setDocumentNames(updatedNames);
 	};
 
-	// Handle form submission
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 		setSubmitting(true);
 
-		// Ensure that files and documentNames arrays have the same length
-		if (files.length !== documentNames.length) {
-			toast.error("The number of files must match the number of document names.");
-			setSubmitting(false);
-			return;
-		}
-
 		const formData = new FormData();
 
-		// Append files and their corresponding document names
 		files.forEach((file, index) => {
-			formData.append("files", file); // append each file
-			formData.append("documentNames[]", documentNames[index]); // append each corresponding document name
+			formData.append("files", file);
+			formData.append("documentNames[]", documentNames[index]);
 		});
 
 		formData.append("categories", categories);
@@ -182,278 +174,289 @@ const CompleteRegistration = () => {
 		formData.append("businessDetails", JSON.stringify(businessDetails));
 
 		try {
-			// Call the API to upload files and form data
-			await updateProfile(userId, formData);
+			await updateProfile(vendorId, formData);
 			setSubmitting(false);
-			toast.success("Profile updated successfully");
-			navigate("/user/login?vendor=true");
+			setModalOpen(true); // Open the modal on successful submit
 		} catch (error) {
 			toast.error("Failed to update profile.");
-			console.error("Failed to update profile:", error);
 			setSubmitting(false);
 		}
 	};
 
+	const goToLogin = () => {
+		setModalOpen(false);
+		navigate("/user/login?vendor=true");
+	};
+
+	const renderStep = () => {
+		switch (step) {
+			case 1:
+				return (
+					<Card>
+						<CardHeader title="Contact Person" />
+						<CardContent>
+							<Grid container spacing={3}>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Name"
+										name="name"
+										value={contactPerson.name}
+										onChange={handleContactPersonChange}
+										fullWidth
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Email"
+										name="email"
+										value={contactPerson.email}
+										onChange={handleContactPersonChange}
+										fullWidth
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Phone Number"
+										name="phoneNumber"
+										value={contactPerson.phoneNumber}
+										onChange={handleContactPersonChange}
+										fullWidth
+										required
+									/>
+								</Grid>
+							</Grid>
+						</CardContent>
+					</Card>
+				);
+			case 2:
+				return (
+					<Card>
+						<CardHeader title="Account Details" />
+						<CardContent>
+							<Grid container spacing={3}>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Holder Name"
+										name="holderName"
+										value={accountDetails.holderName}
+										onChange={handleAccountDetailsChange}
+										fullWidth
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Account Number"
+										name="number"
+										value={accountDetails.number}
+										onChange={handleAccountDetailsChange}
+										fullWidth
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Bank Name"
+										name="bankName"
+										value={accountDetails.bankName}
+										onChange={handleAccountDetailsChange}
+										fullWidth
+										required
+									/>
+								</Grid>
+							</Grid>
+						</CardContent>
+					</Card>
+				);
+			case 3:
+				return (
+					<Card>
+						<CardHeader title="Business Details" />
+						<CardContent>
+							<Grid container spacing={3}>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Business Name"
+										name="businessName"
+										value={businessDetails.businessName}
+										onChange={handleBusinessDetailsChange}
+										fullWidth
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="CAC Registration Number"
+										name="cacRegNumber"
+										value={businessDetails?.cacRegNumber}
+										onChange={handleBusinessDetailsChange}
+										fullWidth
+										margin="normal"
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="TIN Number"
+										name="tinNumber"
+										value={businessDetails?.tinNumber}
+										onChange={handleBusinessDetailsChange}
+										fullWidth
+										margin="normal"
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="RC Number"
+										name="rcNumber"
+										value={businessDetails?.rcNumber}
+										onChange={handleBusinessDetailsChange}
+										fullWidth
+										margin="normal"
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Registered Address"
+										name="registeredAddress"
+										value={businessDetails?.registeredAddress}
+										onChange={handleBusinessDetailsChange}
+										fullWidth
+										margin="normal"
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Physical Address"
+										name="physicalAddress"
+										value={businessDetails?.physicalAddress}
+										onChange={handleBusinessDetailsChange}
+										fullWidth
+										margin="normal"
+										required
+									/>
+								</Grid>
+								<Grid item xs={12} md={6}>
+									<TextField
+										label="Functional Mail Address"
+										name="functionalMailAddress"
+										value={businessDetails?.functionalMailAddress}
+										onChange={handleBusinessDetailsChange}
+										fullWidth
+										margin="normal"
+										required
+									/>
+								</Grid>
+							</Grid>
+						</CardContent>
+					</Card>
+				);
+			case 4:
+				return (
+					<Card>
+						<CardHeader title="Categories" />
+						<CardContent>
+							<Select
+								multiple
+								value={categories}
+								onChange={handleCategoriesChange}
+								input={<OutlinedInput />}
+								renderValue={(selected) => (
+									<Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+										{selected.map((value) => (
+											<Chip key={value} label={value} />
+										))}
+									</Box>
+								)}
+								fullWidth
+							>
+								{uniqueCategories.map((category) => (
+									<MenuItem key={category} value={category}>
+										{category}
+									</MenuItem>
+								))}
+							</Select>
+						</CardContent>
+					</Card>
+				);
+			case 5:
+				return (
+					<Card>
+						<CardHeader title="File Upload" />
+						<CardContent>
+							<input type="file" multiple onChange={handleFileChange} />
+							{files.map((file, index) => (
+								<Box key={index} sx={{ display: "flex", alignItems: "center" }}>
+									<TextField
+										label="Document Name"
+										value={documentNames[index]}
+										onChange={(e) => handleDocumentNameChange(index, e)}
+									/>
+									<IconButton onClick={() => handleRemoveFile(index)}>
+										<DeleteIcon />
+									</IconButton>
+								</Box>
+							))}
+						</CardContent>
+					</Card>
+				);
+			default:
+				return null;
+		}
+	};
+
 	return (
-		<Container sx={{ my: 4, px: 5, mx: "auto" }}>
+		<Container sx={{ my: 4, px: 5 }}>
 			<Typography variant="h4" gutterBottom>
 				Complete Registration
 			</Typography>
-			<Typography variant="body2" gutterBottom sx={{ my: 3 }}>
-				Wellcome {contactPerson?.name} to the registration page. Please complete
-				your registration by filling the form below.
-			</Typography>
 
-			{/* Contact Person Card */}
-			<Card sx={{ mt: 3 }}>
-				<CardHeader title="Contact Person" />
-				<CardContent>
-					<Box>
-						<Grid container spacing={3}>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Name"
-									name="name"
-									value={contactPerson?.name}
-									onChange={handleContactPersonChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Email"
-									name="email"
-									type="email"
-									value={contactPerson?.email}
-									onChange={handleContactPersonChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Phone Number"
-									name="phoneNumber"
-									type="tel"
-									value={contactPerson?.phoneNumber}
-									onChange={handleContactPersonChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-						</Grid>
-					</Box>
-				</CardContent>
-			</Card>
-			{/* Account Details Card */}
-			<Card sx={{ mt: 3 }}>
-				<CardHeader title="Account Details" />
-				<CardContent>
-					<Box>
-						<Grid container spacing={3}>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Holder Name"
-									name="holderName"
-									value={accountDetails?.holderName}
-									onChange={handleAccountDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Account Number"
-									name="number"
-									value={accountDetails?.number}
-									onChange={handleAccountDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Bank Name"
-									name="bankName"
-									value={accountDetails?.bankName}
-									onChange={handleAccountDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-						</Grid>
-					</Box>
-				</CardContent>
-			</Card>
+			{renderStep()}
 
-			{/* Business Details Card */}
-			<Card sx={{ mt: 3 }}>
-				<CardHeader title="Business Details" />
-				<CardContent>
-					<Box>
-						<Grid container spacing={3}>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Business Name"
-									name="businessName"
-									value={businessDetails?.businessName}
-									onChange={handleBusinessDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="CAC Registration Number"
-									name="cacRegNumber"
-									value={businessDetails?.cacRegNumber}
-									onChange={handleBusinessDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="TIN Number"
-									name="tinNumber"
-									value={businessDetails?.tinNumber}
-									onChange={handleBusinessDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="RC Number"
-									name="rcNumber"
-									value={businessDetails?.rcNumber}
-									onChange={handleBusinessDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Registered Address"
-									name="registeredAddress"
-									value={businessDetails?.registeredAddress}
-									onChange={handleBusinessDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Physical Address"
-									name="physicalAddress"
-									value={businessDetails?.physicalAddress}
-									onChange={handleBusinessDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<TextField
-									label="Functional Mail Address"
-									name="functionalMailAddress"
-									value={businessDetails?.functionalMailAddress}
-									onChange={handleBusinessDetailsChange}
-									fullWidth
-									margin="normal"
-									required
-								/>
-							</Grid>
-						</Grid>
-					</Box>
-				</CardContent>
-			</Card>
-
-			{/* Categories Card */}
-			<Card sx={{ mt: 3 }}>
-				<CardHeader title="Categories" />
-				<CardContent>
-					<Box>
-						<Select
-							label="Categories"
-							multiple
-							value={categories}
-							onChange={handleCategoriesChange}
-							input={<OutlinedInput label="Categories" />}
-							renderValue={(selected) => (
-								<Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-									{selected.map((value) => (
-										<Chip key={value} label={value} />
-									))}
-								</Box>
-							)}
-							fullWidth
-						>
-							{uniqueCategories.map((category) => (
-								<MenuItem key={category} value={category}>
-									{category}
-								</MenuItem>
-							))}
-						</Select>
-					</Box>
-				</CardContent>
-			</Card>
-
-			{/* Multiple File Upload Card with dynamic fields  for the name of the document */}
-			<Card sx={{ mt: 3 }}>
-				<CardHeader title="Upload Files (e.g. Certificates, Licenses, Registrations, TIN)" />
-				<CardContent>
-					<Box>
-						{/* File Input */}
-						<input
-							type="file"
-							multiple
-							onChange={handleFileChange}
-							style={{ marginBottom: "16px" }}
-						/>
-
-						{/* Dynamic Fields for Document Names */}
-						{files.map((file, index) => (
-							<Box key={index} sx={{ display: "flex", alignItems: "center", mb: 2 }}>
-								<TextField
-									label={`Document Name for ${file.name}`}
-									value={documentNames[index]}
-									onChange={(e) => handleDocumentNameChange(index, e)}
-									fullWidth
-									required
-								/>
-								<IconButton onClick={() => handleRemoveFile(index)} sx={{ ml: 2 }}>
-									<DeleteIcon />
-								</IconButton>
-							</Box>
-						))}
-					</Box>
-				</CardContent>
-			</Card>
-
-			{/* Submit Button */}
-			<Box sx={{ mt: 3 }}>
+			<Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
 				<Button
 					variant="contained"
-					color="primary"
-					onClick={handleSubmit}
-					disabled={submitting}
+					onClick={() => setStep(step - 1)}
+					disabled={step === 1}
 				>
-					{submitting ? "Submitting..." : "Submit"}
+					Back
 				</Button>
+				{step === 5 ? (
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={handleSubmit}
+						disabled={submitting}
+					>
+						{submitting ? "Submitting..." : "Submit"}
+					</Button>
+				) : (
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={() => setStep(step + 1)}
+					>
+						Next
+					</Button>
+				)}
 			</Box>
+			<Modal open={isModalOpen} onClose={() => setModalOpen(false)}>
+				<Card sx={{ p: 3, mx: "auto", my: 5, maxWidth: 400 }}>
+					<CardContent>
+						<Typography variant="h5" gutterBottom>
+							Profile Completed!
+						</Typography>
+						<Button variant="contained" color="primary" onClick={goToLogin}>
+							Go to Login
+						</Button>
+					</CardContent>
+				</Card>
+			</Modal>
 		</Container>
 	);
 };
